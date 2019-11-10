cc39dbb2dab2ff429cee579e9fd56910512b2a87
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-10 05:58:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,5 +2,5 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-name_hash.each.collect {|a, b| 
+name_hash.each.collect {|a, b| a[:apr] <=> b[:apr] }
 end
