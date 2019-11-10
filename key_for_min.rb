5ccf49f39257e091e7e015501c37011553d3f696
Taylor Kraft <taylorkraft16@gmail.com>
2019-11-10 06:12:27 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,12 +7,12 @@ def key_for_min_value(name_hash)
   else
       min_value = name_hash.first[1]
       min_key = name_hash.first[0]
-      name_hash.each.collect do |key, value|
+      name_hash.each.collect { |key, value|
         if value < min_value
           min_value = value
           min_key = key
         end
-      end
+      }
       min_key
   end 
 end
